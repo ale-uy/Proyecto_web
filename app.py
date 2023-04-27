4171b2b8a68ca94ce2744084a4ba347dd373380c
BuildTools <unconfigured@null.spigotmc.org>
2023-04-27 02:47:49 -0300

    frozen add

---
+++ app.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+from flask import Flask, render_template
+from brou_api import scraper_brou
+from brou_api import scraper_bs
+from flask_frozen import Freezer
+
+app = Flask(__name__, static_folder="static")
+app.static_url_path = "/static"
+freezer = Freezer(app)
+
+@app.route("/")
+def index():
+    title = 'Mi Web Retro tipo Matrix'
+    return render_template("index.html", title=title)
+
+
+@app.get("/brou/v1")
+def api_brou():
+    try:
+        datos = scraper_brou.query()
+        return datos
+    except Exception as e:
+        return 'No funciona el scraper, error: {}'.format(e)
+
+@app.get("/brou/v2")
+def api_brou_2():
+    try:
+        datos = scraper_bs.query()
+        return datos
+    except Exception as e:
+        return 'No funciona el scraper, error: {}'.format(e)
+        
+@freezer.register_generator
+def index_route():
+    yield '/'
+
+if __name__ == "__main__":
+    freezer.freeze()
